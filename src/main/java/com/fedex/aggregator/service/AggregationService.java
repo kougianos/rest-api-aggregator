26dee4720ab1b22f3bf8a1916f1c38e7b9a05050
Nikos Kougianos <nikolaos.kougianos@betvictor.com>
2024-02-25 21:06:19 +0200

    impl

--- src/main/java/com/fedex/aggregator/service/AggregationService.java
+++ src/main/java/com/fedex/aggregator/service/AggregationService.java
@@ -2,45 +2,72 @@ package com.fedex.aggregator.service;
 
 import com.fedex.aggregator.dto.GenericMap;
 import com.fedex.aggregator.util.FedexUtils;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static com.fedex.aggregator.dto.Constants.*;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class AggregationService {
 
     private final ExternalApiClient client;
+    private final ConcurrentMap<String, BlockingQueue<String>> apiQueues;
+    private static final int QUEUE_SIZE = 5;
+    private final ConcurrentMap<String, GenericMap> apiResponses = new ConcurrentHashMap<>();
+
+    public AggregationService(ExternalApiClient client) {
+        this.client = client;
+        this.apiQueues = new ConcurrentHashMap<>();
+        this.apiQueues.put(PRICING, new LinkedBlockingQueue<>());
+        this.apiQueues.put(TRACK, new LinkedBlockingQueue<>());
+        this.apiQueues.put(SHIPMENTS, new LinkedBlockingQueue<>());
+
+        apiResponses.put(PRICING, new GenericMap());
+        apiResponses.put(TRACK, new GenericMap());
+        apiResponses.put(SHIPMENTS, new GenericMap());
+    }
 
     public Mono<Map<String, GenericMap>> getAggregatedResponse(Map<String, String> parameters) {
 
         List<Mono<Entry<String, GenericMap>>> monoList = new ArrayList<>();
-        parameters.forEach((apiName, params) -> callExternalApiAndPopulateList(monoList, apiName, params));
+        parameters.forEach((apiName, params) -> {
+            BlockingQueue<String> queue = apiQueues.get(apiName);
+            var paramList = Arrays.stream(params.split(",")).distinct().toList();
+
+            paramList.forEach(param -> {
+                if (!queue.contains(param)) {
+                    queue.add(param);
+                }
+            });
+
+            executeApiCall(monoList, apiName, queue);
+
+        });
+
         Mono<List<Entry<String, GenericMap>>> zippedMono = zipApiResponses(monoList);
 
         return zippedMono.map(this::transformToAggregatedResponse);
     }
 
-    private void callExternalApiAndPopulateList(List<Mono<Entry<String, GenericMap>>> monoList, String apiName,
-                                                String params) {
-        var paramList = Arrays.stream(params.split(",")).distinct();
-        paramList.forEach(param -> {
-            var apiCallMono = client.get(apiName, param, GenericMap.class)
-                .defaultIfEmpty(new GenericMap())
-                .map(response -> {
-                    GenericMap field = new GenericMap();
-                    field.put(param, response.get(param));
-                    return Map.entry(apiName, field);
-                });
-            monoList.add(apiCallMono);
-        });
+    private void executeApiCall(List<Mono<Entry<String, GenericMap>>> monoList, String apiName,
+                                BlockingQueue<String> queue) {
+        var p = String.join(",", queue.stream().toList());
+        var apiCallMono = client.get(apiName, p, GenericMap.class)
+            .defaultIfEmpty(new GenericMap())
+            .doOnNext(response -> apiResponses.put(apiName, response))
+            .map(response -> Map.entry(apiName, response));
+
+        monoList.add(apiCallMono);
     }
 
     private Mono<List<Entry<String, GenericMap>>> zipApiResponses(List<Mono<Entry<String, GenericMap>>> monoList) {
@@ -57,14 +84,19 @@ public class AggregationService {
         responseList.forEach(responseEntry -> {
             var apiResponse = responseEntry.getValue();
 
-            aggregatedResponse.get(String.valueOf(responseEntry.getKey()))
-                .put(apiResponse.firstEntry().getKey(), apiResponse.firstEntry().getValue());
+            aggregatedResponse.put(responseEntry.getKey(), apiResponse);
 
         });
 
         FedexUtils.removeEmptyEntriesFromMap(aggregatedResponse);
 
         return aggregatedResponse;
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void x() {
+        log.info("API RESPONSES {}", apiResponses);
+        log.info("QUEUES {}\n", apiQueues);
     }
 
 }
