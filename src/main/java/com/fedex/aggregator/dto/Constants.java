09c485f04fdd8c2d870045047faefbf2475e8e50
Nikos Kougianos <nikolaos.kougianos@betvictor.com>
2024-02-25 14:39:07 +0200

    cleanup

---
+++ src/main/java/com/fedex/aggregator/dto/Constants.java
@@ -0,0 +1,13 @@
+package com.fedex.aggregator.dto;
+
+public final class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("Util class");
+    }
+
+    public static final String PRICING = "pricing";
+    public static final String TRACK = "track";
+    public static final String SHIPMENTS = "shipments";
+
+}
--- src/main/java/com/fedex/aggregator/router/FedexHandler.java
+++ src/main/java/com/fedex/aggregator/router/FedexHandler.java
@@ -1,9 +1,6 @@
 package com.fedex.aggregator.router;
 
-import com.fedex.aggregator.dto.GenericMap;
-import com.fedex.aggregator.dto.PricingResponse;
-import com.fedex.aggregator.dto.ShipmentsResponse;
-import com.fedex.aggregator.dto.TrackResponse;
+import com.fedex.aggregator.dto.*;
 import com.fedex.aggregator.service.ExternalApiClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +10,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
+
+import static com.fedex.aggregator.dto.Constants.*;
 
 @Service
 @Slf4j
@@ -21,51 +21,52 @@ import java.util.stream.Collectors;
 public class FedexHandler {
 
     // can be moved to app properties
-    private static final Map<String, Class<?>> pathToClassMapping = Map.of(
-        "pricing", PricingResponse.class,
-        "track", TrackResponse.class,
-        "shipments", ShipmentsResponse.class);
+    private static final List<String> acceptableParameters = List.of(PRICING, TRACK, SHIPMENTS);
     private final ExternalApiClient client;
 
     public Mono<ServerResponse> getAggregatedResponse(ServerRequest request) {
         log.info("REQUEST: {}", request.queryParams().toSingleValueMap());
         Map<String, String> parameters = cleanQueryParameters(request);
 
-        List<Mono<GenericMap>> monoList = new ArrayList<>();
+        List<Mono<Entry<String, GenericMap>>> monoList = new ArrayList<>();
 
         parameters.forEach((apiName, params) -> {
             List<String> paramList = Arrays.asList(params.split(","));
             paramList.forEach(p -> {
-                Mono<GenericMap> apiCallMono = client.get(apiName, p, GenericMap.class)
+                var apiCallMono = client.get(apiName, p, GenericMap.class)
                     .defaultIfEmpty(new GenericMap())
                     .map(response -> {
                         GenericMap fieldMap = new GenericMap();
                         fieldMap.put(p, response.get(p));
-                        return fieldMap;
+                        return Map.entry(apiName, fieldMap);
                     });
                 monoList.add(apiCallMono);
             });
         });
 
-        Mono<List<GenericMap>> combinedMono = Mono.zip(monoList, objects -> Arrays.stream(objects)
-            .map(GenericMap.class::cast)
+        Mono<List<Entry<String, GenericMap>>> combinedMono = Mono.zip(monoList, objects -> Arrays.stream(objects)
+            .map(obj -> (Entry<String, GenericMap>) obj)
             .toList());
 
         var serverResponse = combinedMono.map(responseList -> {
             Map<String, GenericMap> aggregatedResponse = new HashMap<>();
-            aggregatedResponse.put("track", new GenericMap());
-            aggregatedResponse.put("shipments", new GenericMap());
-            aggregatedResponse.put("pricing", new GenericMap());
-            responseList.forEach(response -> {
-                var entry = response.firstEntry();
-                if (entry.getValue() instanceof String) {
-                    aggregatedResponse.get("track").put(entry.getKey(), entry.getValue());
-                } else if (entry.getValue() instanceof List) {
-                    aggregatedResponse.get("shipments").put(entry.getKey(), entry.getValue());
-                } else if (entry.getValue() instanceof Number) {
-                    aggregatedResponse.get("pricing").put(entry.getKey(), entry.getValue());
+            aggregatedResponse.put(TRACK, new GenericMap());
+            aggregatedResponse.put(SHIPMENTS, new GenericMap());
+            aggregatedResponse.put(PRICING, new GenericMap());
+            responseList.forEach(responseEntry -> {
+                var apiResponse = responseEntry.getValue();
+
+                aggregatedResponse.get(String.valueOf(responseEntry.getKey()))
+                    .put(apiResponse.firstEntry().getKey(), apiResponse.firstEntry().getValue());
+
+            });
+            List<String> keysToRemove = new ArrayList<>();
+            aggregatedResponse.forEach((key, value) -> {
+                if (value.isEmpty()) {
+                    keysToRemove.add(key);
                 }
             });
+            keysToRemove.forEach(aggregatedResponse::remove);
             return aggregatedResponse;
         });
 
@@ -78,8 +79,8 @@ public class FedexHandler {
 
     private Map<String, String> cleanQueryParameters(ServerRequest request) {
         var map = new HashMap<>(request.queryParams());
-        map.keySet().retainAll(pathToClassMapping.keySet());
-        return map.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().get(0)));
+        map.keySet().retainAll(acceptableParameters);
+        return map.entrySet().stream().collect(Collectors.toMap(Entry::getKey, e -> e.getValue().get(0)));
     }
 
 }
