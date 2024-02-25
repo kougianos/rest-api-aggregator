6960581e77c7ea010c3f71945a1ff8145988d3c7
Nikos Kougianos <nikolaos.kougianos@betvictor.com>
2024-02-25 03:03:09 +0200

    tmp commit

--- src/main/java/com/fedex/aggregator/dto/PricingResponse.java
+++ src/main/java/com/fedex/aggregator/dto/PricingResponse.java
@@ -3,5 +3,5 @@ package com.fedex.aggregator.dto;
 import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 
-public class PricingResponse extends GenericMap {
+public class PricingResponse extends LinkedHashMap<String, BigDecimal> {
 }
