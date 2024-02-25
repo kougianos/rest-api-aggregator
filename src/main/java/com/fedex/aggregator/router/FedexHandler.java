77573538865abcba9868fb44416211afe36d7d7b
Nikos Kougianos <nikolaos.kougianos@betvictor.com>
2024-02-25 03:55:39 +0200

    implement story 1, TODO include null values in response

--- src/main/java/com/fedex/aggregator/router/FedexHandler.java
+++ src/main/java/com/fedex/aggregator/router/FedexHandler.java
@@ -1,13 +1,15 @@
 package com.fedex.aggregator.router;
 
-import com.fedex.aggregator.dto.*;
+import com.fedex.aggregator.dto.GenericMap;
+import com.fedex.aggregator.dto.PricingResponse;
+import com.fedex.aggregator.dto.ShipmentsResponse;
+import com.fedex.aggregator.dto.TrackResponse;
 import com.fedex.aggregator.service.ExternalApiClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.*;
@@ -29,15 +31,13 @@ public class FedexHandler {
         log.info("REQUEST: {}", request.queryParams().toSingleValueMap());
         Map<String, String> parameters = cleanQueryParameters(request);
 
-        // Create a list to hold all the Mono instances for API calls
         List<Mono<GenericMap>> monoList = new ArrayList<>();
 
         parameters.forEach((apiName, params) -> {
             List<String> paramList = Arrays.asList(params.split(","));
-            // Create a Mono for each API call and add it to the list
             paramList.forEach(p -> {
                 Mono<GenericMap> apiCallMono = client.get(apiName, p, GenericMap.class)
-                    .defaultIfEmpty(new GenericMap()) // To handle null responses
+                    .defaultIfEmpty(new GenericMap())
                     .map(response -> {
                         GenericMap fieldMap = new GenericMap();
                         fieldMap.put(p, response.get(p));
@@ -47,12 +47,32 @@ public class FedexHandler {
             });
         });
 
-        // Combine all Monos in the list into one Mono using Flux.zip
         Mono<List<GenericMap>> combinedMono = Mono.zip(monoList, objects -> Arrays.stream(objects)
             .map(GenericMap.class::cast)
             .toList());
 
-        return combinedMono.flatMap(c -> ServerResponse.ok().bodyValue(c));
+        var serverResponse = combinedMono.map(responseList -> {
+            Map<String, GenericMap> aggregatedResponse = new HashMap<>();
+            aggregatedResponse.put("track", new GenericMap());
+            aggregatedResponse.put("shipments", new GenericMap());
+            aggregatedResponse.put("pricing", new GenericMap());
+            responseList.forEach(response -> {
+                var entry = response.firstEntry();
+                if (entry.getValue() instanceof String) {
+                    aggregatedResponse.get("track").put(entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof List) {
+                    aggregatedResponse.get("shipments").put(entry.getKey(), entry.getValue());
+                } else if (entry.getValue() instanceof Number) {
+                    aggregatedResponse.get("pricing").put(entry.getKey(), entry.getValue());
+                }
+            });
+            return aggregatedResponse;
+        });
+
+        return serverResponse.flatMap(resp -> {
+            log.info("RESPONSE: {}\n", resp);
+            return ServerResponse.ok().bodyValue(resp);
+        });
 
     }
 
