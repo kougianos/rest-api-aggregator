f158e0d7b9875c8d36f62be38a9ea997a23eed18
Nikos Kougianos <nikolaos.kougianos@betvictor.com>
2024-03-05 12:07:54 +0200

    add test scenario

--- src/test/java/com/fedex/aggregator/service/FedexIntegrationTests.java
+++ src/test/java/com/fedex/aggregator/service/FedexIntegrationTests.java
@@ -2,7 +2,6 @@ package com.fedex.aggregator.service;
 
 import com.fedex.aggregator.dto.GenericMap;
 import lombok.SneakyThrows;
-import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,20 +14,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "36000")
@@ -105,6 +104,55 @@ class FedexIntegrationTests {
                 assertEquals(expectedResponse, response);
             });
 
+        verify((externalApiClient), times(1)).get(anyString(), anyString());
+        queueManager.getApiQueues().values().forEach(queue -> assertTrue(queue.isEmpty()));
+
+    }
+
+    @Test
+    void testAllParameters_OK() {
+        var trackResponse = new GenericMap();
+        trackResponse.put("1", "DELIVERING");
+        trackResponse.put("2", "COLLECTING");
+        trackResponse.put("3", "COLLECTED");
+        trackResponse.put("4", "NEW");
+        trackResponse.put("5", "NEW");
+        Mockito.when(externalApiClient.get("track", "1,2,3,4,5"))
+            .thenReturn(Mono.just(trackResponse));
+
+        var pricingResponse = new GenericMap();
+        pricingResponse.put("DE", 38.501620847664405);
+        pricingResponse.put("CH", 1.8747583717610317);
+        pricingResponse.put("CN", 94.16635949653589);
+        pricingResponse.put("GB", 99.50306403621781);
+        pricingResponse.put("NL", 53.786320622647764);
+        Mockito.when(externalApiClient.get("pricing", "NL,CN,CH,GB,DE"))
+            .thenReturn(Mono.just(pricingResponse));
+
+        var shipmentsResponse = new GenericMap();
+        shipmentsResponse.put("1", List.of("envelope"));
+        shipmentsResponse.put("2", List.of("box", "pallet"));
+        shipmentsResponse.put("3", List.of("envelope", "envelope", "pallet"));
+        shipmentsResponse.put("4", List.of("pallet", "envelope", "envelope", "pallet"));
+        shipmentsResponse.put("5", List.of("box", "box", "envelope", "pallet", "envelope"));
+        Mockito.when(externalApiClient.get("shipments", "1,2,3,4,5"))
+            .thenReturn(Mono.just(shipmentsResponse));
+
+        var expectedResponse = new GenericMap();
+        expectedResponse.put("track", trackResponse);
+        expectedResponse.put("pricing", pricingResponse);
+        expectedResponse.put("shipments", shipmentsResponse);
+
+        webTestClient.get()
+            .uri("/aggregation?pricing=NL,CN,CH,GB,DE&track=1,2,3,4,5&shipments=1,2,3,4,5")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(GenericMap.class)
+            .value(response -> {
+                assertEquals(expectedResponse, response);
+            });
+
+        verify((externalApiClient), times(3)).get(anyString(), anyString());
         queueManager.getApiQueues().values().forEach(queue -> assertTrue(queue.isEmpty()));
 
     }
